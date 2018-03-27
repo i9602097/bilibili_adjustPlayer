fb244c4b30463aef61aff2d96859e0a0c878cda1
i9602097 <i9602097@outlook.com>
2018-03-27 13:06:14 +0800

    修正播放器過矮時出現"空心"現象
    後補圖

--- bilibili_adjustPlayer.user.js
+++ bilibili_adjustPlayer.user.js
@@ -12,7 +12,7 @@
 // @include     http*://bangumi.bilibili.com/movie/*
 // @exclude     http*://bangumi.bilibili.com/movie/
 // @description 调整B站播放器设置，增加一些实用的功能。
-// @version     1.18
+// @version     1.18.1
 // @grant       GM.setValue
 // @grant       GM_setValue
 // @grant       GM.getValue
@@ -419,6 +419,7 @@
 						'#bofqi .player, .moviescontent { width: '+ width +' !important; height: calc(48px + '+ width +' / calc('+ ratio +') - 300px / calc('+ ratio +') + 68px) !important; } ',
 						'#bofqi.wide .player, .wide.moviescontent { width: '+ width +' !important; height: calc('+ width +' / calc('+ ratio +') + 68px) !important; } ',
 						'.player-wrapper .bangumi-player, #__bofqi.bili-wrapper { width: '+ width +' !important; background: none !important; height: auto !important;} ',
+						'#bangumi_player.player-wrapper, #__bofqi.bili-wrapper { min-height: unset !important;} ',
 						'#bofqi.wide .autohide-controlbar, .wide.autohide-controlbar-movies { width: '+ width +' !important; height: calc('+ width +' / calc('+ ratio +') + 0px) !important; } '
 					];
 					var node = document.createElement('style');
